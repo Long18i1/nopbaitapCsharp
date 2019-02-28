25b077e3c704ea4b543cca3afdb3fd730d296bff
Long18i1 <48077478+Long18i1@users.noreply.github.com>
2019-02-28 10:40:43 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baikiemtra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tinhtong tong = new Tinhtong();
+            tong.tinh();
+            tong.inra();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tinhtong.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baikiemtra
+{
+    class Tinhtong
+    {
+  
+        int i = 0;
+        int n = 0;
+        double a ,b, c;
+        public void tinh()
+        {
+            Console.WriteLine("Nhap n =");
+            this.n = Convert.ToInt32(Console.ReadLine());
+        }
+        public double cauA()
+        {
+            for (int i = 1; i <= n; i++)
+            
+                if (i % 2 == 0)
+                    a -= i;
+                else
+                    a += i;
+
+            return a;
+
+        }
+        public double cauB()
+        {
+            for ( i = 1; i <= n; i++)
+            {
+                b += i / (float)i;
+            }
+            return b;
+            
+        }
+        public double cauC()
+        {
+            for (i = 1; i <= n; i++)
+            {
+                c += i;
+            }
+            return c;
+        }
+
+        public void inra()
+        {
+            Console.WriteLine(" Tong cua cau A = {0}\n Tong cua cau B = {1}\n Tong cua cau C = {2}", this.cauA(),this.cauB(),this.cauC());
+        }
+
+    }
+}
